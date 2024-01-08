3d6b5f79db8bd1aaf8038c667b9d82cdb18546eb
wangji0420 <wangji@shoppal.ai>
2024-01-08 08:42:47 +0000

    remote requirements-dev

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,4 @@
 source /home/vscode/venv/bin/activate
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt
-pip install -r requirements-dev.txt
 sudo chown vscode:vscode /workspace
